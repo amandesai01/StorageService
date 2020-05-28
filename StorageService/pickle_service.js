999db0519b95748134f4f76ac484d73e66471f43
Aman Desai <amandesai01@gmail.com>
2020-05-28 17:17:34 +0530

    Random Populating Done

--- StorageService/pickle_service.js
+++ StorageService/pickle_service.js
@@ -2,11 +2,34 @@ var fs = require('fs')
 var config = require('./config')
 
 var create_allocations = async () => {
+    let available_servers = []
+    available_servers.push({
+        "id" : "A1"
+    })
+    available_servers.push({
+        "id" : "A2"
+    })
+    available_servers.push({
+        "id" : "A3"
+    })
+    available_servers.push({
+        "id" : "A4"
+    })
+    available_servers.push({
+        "id" : "A5"
+    })
+    available_servers.push({
+        "id" : "A6"
+    })
     var obj = {
         storages : []
     }
     for(var i = 0; i < config.MAX_SERVERS; i++){
         obj.storages.push(null)
+    }
+    for(var i = 0 ; i < available_servers.length; i++){
+        let some_random_index = Math.floor((Math.random() * config.MAX_SERVERS) + 1)
+        obj.storages[some_random_index] = available_servers[i]
     }
     fs.writeFileSync("./allocation_data.json", JSON.stringify(obj))
 }

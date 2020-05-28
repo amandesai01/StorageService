7ed3814ce2124af111a3543b5f045cdcfed1b92f
Aman Desai <amandesai01@gmail.com>
2020-05-28 16:09:37 +0530

    Storage Config Done

--- StorageService/UploadService/storage_config.js
+++ StorageService/UploadService/storage_config.js
@@ -1,11 +1,21 @@
 var multer = require('multer')
+var hasher = require('../Hasher')
+var mapper = require('../Mapper')
 
 var storage = multer.diskStorage({
     destination : (req, file, cb) => {
-
+        let path = req.body.path
+        let hashval = await hasher.hash(path)
+        let destination = await mapper.map(hashval)
+        cb(null, destination)
     },
     filename: (req, file, cb) => {
-
+        let path = req.body.path
+        let path_array = path.split('/')
+        path_array.shift()
+        path_array.shift()
+        let filename = await hasher.hash(path_array.join("/"))
+        cb(null, filename)
     }
 });
 

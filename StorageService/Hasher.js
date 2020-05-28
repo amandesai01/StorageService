b38f16a3c08888dea7ce0a5ec093bb3303b83c74
Aman Desai <amandesai01@gmail.com>
2020-05-28 16:12:14 +0530

    Hasher Done

--- StorageService/Hasher.js
+++ StorageService/Hasher.js
@@ -1,1 +1,9 @@
-
+var hash = async (input_string) => {
+    var hash = 0, i, chr;
+    for (i = 0; i < input_string.length; i++) {
+        chr   = input_string.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}

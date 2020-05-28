aa9d63feea84e6c90dcd15005d37fa4ace6a7f7f
Aman Desai <amandesai01@gmail.com>
2020-05-28 21:28:50 +0530

    BETA Mapper Ready

--- StorageService/Mapper.js
+++ StorageService/Mapper.js
@@ -2,10 +2,19 @@ var pickle_service = require('./pickle_service')
 var config = require('./config')
 
 var map = async (hashval) => {
-    let allocations = await pickle_service.get_allocations()
+    let allocations = await pickle_service.get_allocations();
     if (allocations){
-        allocations = await pickle_service.create_allocations()
+        allocations = await pickle_service.create_allocations();
     }
-    
+    let index = hashval % config.MAX_SERVERS;
+    if(!allocations.storages[index]){
+        for(var i = 0; i < allocations.storages.length; i++){
+            if(allocations.storages[i]){
+                index = i
+                break;
+            }
+        }
+    }
+    return allocations.storages[index]
 
 }
